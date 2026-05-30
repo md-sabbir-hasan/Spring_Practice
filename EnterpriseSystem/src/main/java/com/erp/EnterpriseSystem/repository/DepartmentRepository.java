2a690a612000be7ca6fac9aa900f0a4ff58ec605
MD. SABBIR HASAN <sabbirhasan.stat@gmail.com>
2026-05-30 06:09:31 +0600

    dd

--- EnterpriseSystem/src/main/java/com/erp/EnterpriseSystem/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.erp.EnterpriseSystem.repository;
-
-import com.erp.EnterpriseSystem.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
--- EnterpriseSystem/src/main/java/com/erp/EnterpriseSystem/repository/PoliceStationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.erp.EnterpriseSystem.repository;
-
-import com.erp.EnterpriseSystem.entity.PoliceStation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PoliceStationRepository extends JpaRepository<PoliceStation, Long> {
-}
